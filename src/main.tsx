74948e8a198b4907ff780b90c14710900972798a
Aniket Vishwakarma <103020381+Aniketvish0@users.noreply.github.com>
2025-03-29 22:01:33 +0530

    fix: pre rate limit

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,11 @@
 import  { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { PostHogProvider } from 'posthog-js/react';
+import './index.css'
 
-const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY} options={options}>
       <App />
-    </PostHogProvider>
   </StrictMode>
 );
