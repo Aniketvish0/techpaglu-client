89abe3aa2d279a66b9caa7180600088ee0f2c486
Aniket Vishwakarma <103020381+Aniketvish0@users.noreply.github.com>
2025-04-02 22:56:36 +0530

    init: google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import  { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'
-
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-HJRLRR66P3");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
